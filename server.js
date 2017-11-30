4a8d72f221f6b4aeae6992060546f668c7efb7b1
Adam Haag <adam.m.h@gmail.com>
2017-11-30 09:03:55 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Serve up static assets
+app.use(express.static("client/build"));
+
+
+app.get("/", function(req, res){
+  res.send({someData: 1234});
+})
+
+
+// Set up promises with mongoose
+
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
